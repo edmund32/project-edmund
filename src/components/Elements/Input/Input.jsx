1baeb654971f57c16835b1f41a1efa5a40f7b995
Edmund <edmundbanyu08@gmail.com>
2025-11-19 09:45:16 +0700

    minor revision search bar

--- src/components/Elements/Input/Input.jsx
+++ src/components/Elements/Input/Input.jsx
@@ -17,7 +17,7 @@ const Input = forwardRef((props, ref) => {
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className={`text-md rounded-lg w-full py-2 px-3 text-black placeholder:text-slate-400
+        className={`!h-11.25 !py-2 text-md rounded-lg w-full py-2 px-3 text-black placeholder:text-slate-400
           focus:outline-none focus:border-indigo-400 focus:ring-1 focus:ring-indigo-400 ${className} 
           ${isDarkMode ? "bg-slate-800 text-white" : "bg-white text-black"}`}
       />
